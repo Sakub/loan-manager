c077c64fff8dfcb3bd4c01c4b9b867069d6eb823
SakubDev <sandomierzj@gmail.com>
2022-07-13 22:31:18 +0200

    Make particles background visible on all views

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,78 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import { loadFull } from "tsparticles";
+import { ref } from "vue";
+
+const particlesInit = async (engine) => {
+  await loadFull(engine);
+
+  setTimeout(() => {
+    const particlesCanvas = document.querySelector("canvas");
+    if (particlesCanvas) {
+      particlesCanvas.style.zIndex = "-1";
+      particlesCanvas.style.backgroundColor = "";
+    }
+  }, 100);
+};
+
+const particlesOptions = ref({
+  background: {
+    color: {
+      value: "transparent",
+    },
+  },
+  particles: {
+    color: {
+      value: "#00BFA6",
+    },
+    links: {
+      color: "#00BFA6",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outModes: {
+        default: "bounce",
+      },
+      random: true,
+      speed: 3,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 50,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      value: { min: 1, max: 5 },
+    },
+  },
+  detectRetina: true,
+});
 </script>
 
 <template>
+  <Particles
+    style="z-index: -1 !important"
+    id="tsparticles"
+    :particlesInit="particlesInit"
+    :options="particlesOptions"
+  />
   <RouterView />
 </template>
 
--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -5,12 +5,6 @@
   />
 
   <img src="../assets/welcomeScreen.svg" class="view__svg" />
-  <Particles
-    style="z-index: -1 !important"
-    id="tsparticles"
-    :particlesInit="particlesInit"
-    :options="particlesOptions"
-  />
   <Button
     text="LEAVE IT TO US!"
     @click="store.visited"
@@ -26,70 +20,7 @@ import { useFirstVisitStore } from "@/stores/firstVisit";
 import Button from "@/components/Button.vue";
 import ViewHeading from "@/components/ViewHeading.vue";
 
-import { loadFull } from "tsparticles";
-
-const particlesInit = async (engine) => {
-  await loadFull(engine);
-
-  setTimeout(() => {
-    const particlesCanvas = document.querySelector("canvas");
-    if (particlesCanvas) {
-      particlesCanvas.style.zIndex = "-1";
-      particlesCanvas.style.backgroundColor = "";
-    }
-  }, 100);
-};
-
 const store = useFirstVisitStore();
-const particlesOptions = ref({
-  background: {
-    color: {
-      value: "transparent",
-    },
-  },
-  particles: {
-    color: {
-      value: "#00BFA6",
-    },
-    links: {
-      color: "#00BFA6",
-      distance: 150,
-      enable: true,
-      opacity: 0.5,
-      width: 1,
-    },
-    collisions: {
-      enable: true,
-    },
-    move: {
-      direction: "none",
-      enable: true,
-      outModes: {
-        default: "bounce",
-      },
-      random: true,
-      speed: 3,
-      straight: false,
-    },
-    number: {
-      density: {
-        enable: true,
-        area: 800,
-      },
-      value: 50,
-    },
-    opacity: {
-      value: 0.5,
-    },
-    shape: {
-      type: "circle",
-    },
-    size: {
-      value: { min: 1, max: 5 },
-    },
-  },
-  detectRetina: true,
-});
 </script>
 <style scoped lang="scss">
 .view__svg {
